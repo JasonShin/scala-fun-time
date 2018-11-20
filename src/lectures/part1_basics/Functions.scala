3a9cd6ec444727dbed7b5c629b6c1d151f096548
Jason Shin <visualbbasic@gmail.com>
2018-11-20 20:59:54 +1100

    chore: adding some recursion examples

--- src/lectures/part1_basics/Functions.scala
+++ src/lectures/part1_basics/Functions.scala
@@ -22,5 +22,39 @@ object Functions extends App {
 
   def aFunctionWithSideEffect(aString: String): Unit = println("test!")
 
+  def aBigFunction(n: Int): Int = {
+    def aSmall(a: Int, b: Int): Int = a + b
+
+    aSmall(n, n -1)
+  }
+
+  /**
+    * 1. Greeting function (name, age) => "Hi, my name is $name and I am $age years old"
+    * 2. Factorial function 1 * 2 * 3
+    * 3. Fibonacci function
+    *   f(1) = 1 f(2) = 1
+    *   f(n) = f(n - 1) + f(n - 2)
+    *
+    */
+
+  def greet(name: String, age: Int): String = s"Hi, my name is $name and I am $age years old"
+
+  def factorial(n: Int): Int = {
+    if (n < 0) {
+      -1
+    } else if (n == 0) {
+      1
+    } else {
+      n * factorial(n - 1)
+    }
+  }
+
+  def fib(n: Int): Int = {
+    if (n <= 1) {
+      1
+    } else {
+      fib(n - 1) + fib(n - 2)
+    }
+  }
 
 }
